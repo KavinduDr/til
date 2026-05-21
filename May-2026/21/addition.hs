757e72155e9c73e5579db5472722143f1f77df55
KavinduDr <rmkavindudhananjaya@gmail.com>
2026-05-21 23:14:07 +0530

    Add basic arithmetic operations examples in Haskell

---
+++ May-2026/21/addition.hs
@@ -0,0 +1,5 @@
+main = do
+    let var1 = 2
+    let var2 = 3
+    putStrLn "The sum of var1 and var2 is: "
+    print (var1 + var2)
---
+++ May-2026/21/division.hs
@@ -0,0 +1,5 @@
+main = do
+    let var1 = 4
+    let var2 = 2
+    putStrLn "The quotient of var1 and var2 is: "
+    print (var1 / var2)
